df398f87f947125eb0a65a2a9c6f15f3a072c939
Rahul kumar <rksharmaa003@gmail.com>
2025-04-29 10:39:58 +0530

    update

--- savs-frontend/src/index.js
+++ savs-frontend/src/index.js
@@ -1,3 +1,6 @@
+// Move all imports first — NO other code in between
+import { Buffer } from 'buffer';
+import process from 'process';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react';
@@ -6,6 +9,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// After all imports are done, now you can write code
+window.Buffer = Buffer;
+window.process = process;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -13,7 +20,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
