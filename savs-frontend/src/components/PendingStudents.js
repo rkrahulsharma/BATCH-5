99b3f240e2b2163e0ea9c7250de45cdb8b8cc2f5
Rahul kumar <161307607+rkrahulsharma@users.noreply.github.com>
2025-05-10 22:48:23 +0530

    Delete savs-frontend directory

--- savs-frontend/src/components/PendingStudents.js
+++
@@ -1,4 +0,0 @@
-const PendingStudents = ({ admin }) => {
-    return <div><h4>Pending Student Approvals</h4></div>;
-  };
-  export default PendingStudents;
--- savs-frontend/src/components/PerformanceCharts.js
+++
@@ -1,4 +0,0 @@
-const PerformanceCharts = ({ admin }) => {
-  return <div><h4>Performance Analytics (Charts)</h4></div>;
-};
-export default PerformanceCharts;
